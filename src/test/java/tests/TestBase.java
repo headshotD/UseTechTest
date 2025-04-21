6be79c643603f6e09da028f03a3e319b2b1988f1
Oleg <namozov@dev.vtb.ru>
2025-04-21 13:50:23 +0300

    added new page for test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,10 +17,10 @@ public class TestBase {
     static void browserConfiguration() {
 
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        //Configuration.browser = System.getProperty("browser", "chrome");
-        //Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
         Configuration.timeout = 2000;
-        //Configuration.remote = System.getProperty("browserRemote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("browserRemote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
